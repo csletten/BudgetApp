6b6e7943ed66565c8f60be46a8886a31c2af9ce8
Christian Sletten <c.sletten99@gmail.com>
2019-01-22 21:58:16 +0100

    Test

--- BudgetApp/ViewController.swift
+++ BudgetApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     
-    let elements = ["horse", "cat", "dog"]
+    let items = ["horse", "cat", "dog"]
     
     override func viewDidLoad() {
         
@@ -24,7 +24,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return elements.count
+        return items.count
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
@@ -34,8 +34,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         cell.cellView.layer.cornerRadius = cell.cellView.frame.height / 2
         
-        cell.animalLabel.text = elements[indexPath.row]
-        cell.animalImage.image = UIImage(named: elements[indexPath.row])
+        cell.animalLabel.text = items[indexPath.row]
+        cell.animalImage.image = UIImage(named: items[indexPath.row])
         
         cell.animalImage.layer.cornerRadius = cell.cellView.frame.height / 2
         
